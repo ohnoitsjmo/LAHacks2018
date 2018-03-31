e46c2ed1b105e4c88eb714d8a8d888cd6c031fb8
Ryan <rpremi@hotmail.com>
2018-03-31 12:03:45 -0700

    correcting the parsed data and reading it into one large dictionary

--- hashMapScript/idiomHashmap.py
+++ hashMapScript/idiomHashmap.py
@@ -1,13 +1,26 @@
 from string import punctuation
 import json
+import os
+import string
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+j_direct = dir_path[0:-13]+"scraper2/"
+#print(j_direct)
+
+hashmap = dict()
 #create hashmap based on scraped idioms from json file
-with open("idioms-a.json", "r") as file:
-    hashmap = json.load(file)
+for letters in string.ascii_lowercase:
+    cur_file = j_direct+"idioms-"+letters+"2.json"
+    #print(cur_file)
+    with open(cur_file, "r") as file:
+      #  print(file)
+        temp_dict = json.load(file)
+        hashmap.update(temp_dict)
+       # print("sucess" +letters)
 
+#print( hashmap)
 #Input text from the user
 text = "all the more"
-
 
 def main(text):
     text = ''.join(c for c in text if c not in punctuation).lower()
@@ -17,4 +30,4 @@ def main(text):
             return translation
 
 x = main(text)
-print (x)
+#print (x)
