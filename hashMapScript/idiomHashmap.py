14ba4761b14ad6dfb6d55da8f93b34100aaf1e20
RussellCaletena <RussellCaletena@gmail.com>
2018-03-31 08:10:58 -0700

    Minor details to hash script and idioms json

--- hashMapScript/idiomHashmap.py
+++ hashMapScript/idiomHashmap.py
@@ -2,16 +2,19 @@ from string import punctuation
 import json
 
 #create hashmap based on scraped idioms from json file
-
-with open("test.json", "r") as file:
+with open("idioms-a.json", "r") as file:
     hashmap = json.load(file)
 
-text = "Today, it was raining cats and dogs, although a bunch of fives showed up."
+#Input text from the user
+text = "all the more"
+
 
 def main(text):
     text = ''.join(c for c in text if c not in punctuation).lower()
     for phrase in hashmap:
         if phrase in text:
-            print(hashmap[phrase])
+            translation = hashmap[phrase]
+            return translation
 
-main(text)
+x = main(text)
+print (x)
