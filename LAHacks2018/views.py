d371de99bea68d82af9f1129d597cd4bffdcb4f3
RussellCaletena <RussellCaletena@gmail.com>
2018-04-01 01:03:49 -0700

    Hardcode text access to db works

---
+++ LAHacks2018/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+
+# import os
+# os.environ['DJANGO_SETTINGS_MODULE']='LAHacks2018.settings'
+
+import os
+from string import punctuation
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'LAHacks2018.settings'
+application = get_wsgi_application()
+
+from django.http import HttpResponse, JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from FOSTranslator.models import Idiom
+from FOSTranslator.serializers import FOSTranslatorSerializer
+
+
+
+# Create your views here.
+#@csrf_exempt
+def get_idioms(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    ##    if request.method == 'GET':
+            # text_to_check = request.data.text
+    text_to_check = "Today, it's raining cats and dogs and a burden"
+
+    idioms = Idiom.objects.all()
+
+    list_of_dict_idioms = []
+    text_to_check = ''.join(c for c in text_to_check if c not in punctuation).lower()
+
+    for idiom_instance in idioms:
+        #print (idiom_instance.idiom)
+        #print (text_to_check)
+        idiom_instance.idiom = ''.join(c for c in idiom_instance.idiom if c not in punctuation).lower()
+        if idiom_instance.idiom in text_to_check:
+
+            dict2 = {'index': str(text_to_check.find(idiom_instance.idiom)), 'idiom': idiom_instance.idiom, 'literal': idiom_instance.definition}
+            list_of_dict_idioms.append(dict2)
+    print (list_of_dict_idioms)
+
+    return list_of_dict_idioms
+
+one = {}
+one['text'] = "raining cats and dogs"
+two = {}
+two['data'] = one
+two['method'] = "GET"
+x = get_idioms(two)
+
+
+        # serializer = FOSTranslatorSerializer(FOSTranslator, many=True)
+        # return JsonResponse(serializer.data, safe=False)
