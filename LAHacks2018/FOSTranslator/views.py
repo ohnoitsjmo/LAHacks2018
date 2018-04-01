2bb2d0d2a9bddeb0557ece8a9bd0eec8bca65e5e
RussellCaletena <RussellCaletena@gmail.com>
2018-04-01 00:03:58 -0700

    Begin working on recognizing idioms in input text

--- LAHacks2018/FOSTranslator/views.py
+++ LAHacks2018/FOSTranslator/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from FOSTranslator.models import Idiom
+from FOSTranslator.serializers import FOSTranslatorSerializer
+
 # Create your views here.
+@csrf_exempt
+def get_idioms(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET
+        text_to_check = request.data.text
+
+        inputIdiom = FOSTranslator.objects.filter(idiom=text_to_check)
+
+        idioms = FOSTranslator.objects.all()
+        serializer = FOSTranslatorSerializer(FOSTranslator, many=True)
+        return JsonResponse(serializer.data, safe=False)
