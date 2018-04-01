c414df693b96154556d5196e0b3020bc5f776bf2
Selynna Sun <ssun098@gmail.com>
2018-04-01 00:52:47 -0700

    Add instructions for use and photo for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Home = () => (
   <div>
     <h2>Welcome!</h2>
+    <h3>Instructions for use</h3>
+    <p>Have a piece of text you want analyzed for idioms and figures of speech?
+    Go to our <a href="/analyze">analyzer to find out the meaning</a>.</p>
   </div>
 );
 
